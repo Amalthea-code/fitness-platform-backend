50c5be6d6c2d2af5a5d36fb3d96357ea8d04cb1f
dmitriy_krasikov_frontend_dev <fender02297@gmail.com>
2025-12-25 16:42:30 +0300

    v2

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,23 @@
-import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
-import { PrismaClient } from "@prisma/client";
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import "dotenv/config";
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!
+});
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        super({ adapter });
+    }
+
     async onModuleInit() {
-        await this.$connect()
+        await this.$connect();
     }
 
     async onModuleDestroy() {
-        await this.$disconnect()
+        await this.$disconnect();
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
+import { UserRole } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
     constructor(private prisma: PrismaService) {}
 
-    create(data: { email: string; password: string; role: string; }) {
+    create(data: { email: string; password: string; role: UserRole; }) {
         return this.prisma.user.create({ data });
     }
 
